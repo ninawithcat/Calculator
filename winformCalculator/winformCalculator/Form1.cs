00751ea6e9181fc427b65136f5904faa9f5cb535
wangyiling <851326185@qq.com>
2021-08-06 17:54:11 +0800

    小数点第二个0.多是坏的 %result有问题

--- winformCalculator/winformCalculator/Form1.cs
+++ winformCalculator/winformCalculator/Form1.cs
@@ -11,11 +11,12 @@ using System.Windows.Forms;
 namespace winformCalculator
 {
     public partial class Form1 : Form
-    {  
+    {
+        public double decimal1 = 1;   //计数小数（用于两数计算）
+        public double decimal2 = 1;   //计数小数（用于两数计算）
         public double count1 = 0;     //计数器1（用于两数计算）
         public double count2 = 0;     //计数器2（用于两数计算）
         public double result = 0;     //结果
-        public int decimal1 = 0;      //计数小数（用于两数计算）
         public double ts = 0;         //暂存
         public string sign = "";      //符号
 
@@ -34,38 +35,46 @@ namespace winformCalculator
         }
         private void btnNumber_Click(object sender, EventArgs e)        //取值
         {
-            if(((Button)sender).Text =="0")
+            if (sign != "" && count2 ==0)
+                decimal1 = 1;
+            if (txtShow.Text.IndexOf(".") > 0 && sign != "")
             {
-                if (txtShow.Text.IndexOf(".") > 0)
-                {
-                    txtShow.Text = txtShow.Text + "0";
-                    decimal1++;
-                }
+                decimal1 = decimal1 / 10;
             }
-            if (txtShow.Text.IndexOf(".") > 0 && ((Button)sender).Text != "0")
+            if (txtShow.Text.IndexOf(".") > 0 && sign =="")
             {
-                count1 = count1 * 10 + double.Parse(((Button)sender).Text);
-                count1 = count1/Math.Pow(10,decimal1);
-                txtShowCount.Text = "";
-                txtShow.Text = count1.ToString();
-                ts = count1;
-                result = 0;
+                decimal1 = decimal1 / 10;
             }
-            if (sign == "" && ((Button)sender).Text != "0")
+            if (sign == "")
             {
                 if (sign == "" && count1 != 0 && ts == 0)
                 {
                     count1 = 0;
                 }
+                if (decimal1 == 1)
                 count1 = count1 * 10 + double.Parse(((Button)sender).Text);
+                else
+                {
+                    count1 = count1 + double.Parse(((Button)sender).Text) * decimal1;
+                }          
                 txtShowCount.Text = "";
                 txtShow.Text = count1.ToString();
                 ts = count1;
                 result = 0;
+                decimal1 = 1;
             }
-            if (sign != ""  && ((Button)sender).Text != "0")
+            if (sign != "")
             {
-                count2 = count2 * 10 + double.Parse(((Button)sender).Text);
+                if (sign != "" && count2 != 0 && ts == 0)
+                {
+                    count2 = 0;
+                }
+                if (decimal1 == 1)
+                    count2 = count2 * 10 + double.Parse(((Button)sender).Text);
+                else
+                {
+                    count2 = count2 + double.Parse(((Button)sender).Text) * decimal1;
+                }    
                 txtShow.Text = count2.ToString();
             }
         }
@@ -154,12 +163,13 @@ namespace winformCalculator
         }
         private void btnPoint_Click(object sender, EventArgs e)              //小数点
         {
-            if (txtShow.Text == "0")
+            if (txtShow.Text == "0") //&& count1 == 0
             {
+                //decimal1 = 0.1;
                 txtShow.Text = "0.";
-                decimal1 ++;
             }
-            else if (txtShow.Text.IndexOf(".") >= 0)
+
+            else if (txtShow.Text.IndexOf(".") > 0)
             {
                 //MessageBox.Show("已经输入小数点,无须再次输入", "提示");
             }
